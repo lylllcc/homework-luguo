52fac1f5f636ab02145a2d4292f0c40c0e377245
yyljj <yyljj@outlook.com>
2016-04-08 12:34:56 +0800

    go to class .

--- js/app.js
+++ js/app.js
@@ -6,9 +6,16 @@ function imgLocation(parent,content){
 	//parent 内容全部取出 取出box
 	var cparent = document.getElementById(parent);
 	var ccontent = getChildElement(cparent,content);
-	console.log("!2");
-
-}
+	var imgWidth = ccontent[0].offsetWidth;
+	var num = Math.floor(document.documentElement.clientWidth/imgWidth);
+	cparent.style.cssText = "width:"+num*imgWidth+"px;magrin:0 auto;";
+	var boxHightArry = [];
+	for(var i=0 ;i<ccontent.length;i++){
+		
+		boxHightArry[i] = ccontent[i].offsetHeight;
+		console.log(boxHightArry[i]);
+	}
+1}
 function getChildElement(parent,content){
 	var contentArr = [];
 	var allcontent = parent.getElementsByTagName("*");
